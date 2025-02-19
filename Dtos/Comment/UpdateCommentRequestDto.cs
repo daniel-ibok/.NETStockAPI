9d1bec9fcdd087387fd8fa58214086c9a1b863be
daniel-ibok <etimdaniel08@gmail.com>
2025-02-19 17:57:00 +0100

    implemented data validation for comment and stock DTos using data annotation

--- Dtos/Comment/UpdateCommentRequestDto.cs
+++ Dtos/Comment/UpdateCommentRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,14 @@ namespace API.Dtos.Comment
 {
     public class UpdateCommentRequestDto
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Title must be 5 characters")]
+        [MaxLength(280, ErrorMessage = "Title cannot be over 280 characters")]
         public string Title { get; set; } = string.Empty;
 
+        [Required]
+        [MinLength(5, ErrorMessage = "Content must be 5 characters")]
+        [MaxLength(320, ErrorMessage = "Content cannot be over 320 characters")]
         public string Content { get; set; } = string.Empty;
-
-        public int StockId { get; set; }
     }
 }
